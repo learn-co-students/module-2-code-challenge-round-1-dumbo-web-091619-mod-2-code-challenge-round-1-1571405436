232457e8929365bfc112f529212a9a4629c21e82
Oneil <oneilswilliams@gmail.com>
2019-10-18 12:53:19 -0400

    done

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,6 +1,15 @@
 class GuestsController < ApplicationController
 
+  def get_guest
+    @guest = Guest.find(params[:id])
+  end 
+
   def index
     @guests = Guest.all
   end
+
+  def show
+    get_guest
+  end 
+
 end
---
+++ app/models/appearance.rb
@@ -0,0 +1,9 @@
+class Appearance < ApplicationRecord
+  belongs_to :guest
+  belongs_to :episode
+
+  validates :rating, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
+  # validates :rating, presence: true 
+  # validates :episode_id, presence: true 
+  # validates :guest_id, presence: true 
+end
