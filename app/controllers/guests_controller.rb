29193e99a79ced66ff005535921ee22d0361e4c2
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-10-18 11:08:36 -0400

    Added new_guest_path and edit_guest_path

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -7,4 +7,29 @@ class GuestsController < ApplicationController
   def show
     @guest = Guest.find(params[:id])
   end
+
+  def new
+    @guest = Guest.new
+  end
+
+  def create
+    @guest = Guest.create!(guest_params)
+    redirect_to guest_path(@guest)
+  end
+
+  def edit
+    @guest = Guest.find(params[:id])
+  end
+
+  def update
+    @guest = Guest.find(params[:id])
+    @guest.update!(guest_params)
+      redirect_to guest_path(@guest)
+  end
+
+  private
+
+  def guest_params
+    params.require(:guest).permit(:name, :occupation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :appearances
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :guests, only: [:index, :show]
+  resources :guests, only: [:index, :show, :new, :create, :edit, :update,]
   resources :episodes, only: [:index, :show]
   resources :appearances, only: [:new, :create]
 end
